7e34758c8e817cfbfc65ee57cc2c7b7b61172761
josephmangara <josephmangara2@gmail.com>
2023-12-05 18:56:05 +0300

    complete

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,42 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    return 'Hello, programmer!'
+print(greet_programmer())
 
 def greet(name):
-    pass
+    return f"Hello, {name}!"
+print(greet("Guido"))
 
 def greet_with_default(name="programmer"):
-    pass
+    return f"Hello, {name}!"
+print(greet_with_default("Sunny"))
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+print(add(541, 3909))
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+    return number / 2
+print(halve(4564))
+
+# def my_function(param):
+#     print("Running my function")
+#     return param + 1
+# my_function_return = my_function(5)
+# print(my_function_return)
+
+# def say_hi(name):
+#     print(f"Hi there, {name}!")
+
+# print(say_hi("Klopp"))
+
+# def stylish_painter():
+#     best_hairstyle = "Bob Ross"
+#     return "Jean-Michel Basquiat"
+#     return best_hairstyle
+#     print(best_hairstyle)
+
+# print(stylish_painter())
