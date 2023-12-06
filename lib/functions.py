5778fcd959da130eb69049e5b5d4d12130affdd5
josephmangara <josephmangara2@gmail.com>
2023-12-06 11:34:04 +0300

    complete

--- lib/functions.py
+++ lib/functions.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    return 'Hello, programmer!'
+    print('Hello, programmer!')
 print(greet_programmer())
 
 def greet(name):
-    return f"Hello, {name}!"
+    print(f"Hello, {name}!")
 print(greet("Guido"))
 
 def greet_with_default(name="programmer"):
-    return f"Hello, {name}!"
+    print(f"Hello, {name}!")
 print(greet_with_default("Sunny"))
 
 def add(num1, num2):
     return num1 + num2
-print(add(541, 3909))
+add(45, 55)
 
 def halve(number):
-    if not isinstance(number, (int, float)):
-        return None
-    return number / 2
-print(halve(4564))
+    # if not isinstance(number, (int, float)):
+    #     return None
+    return number/2
+halve(4564)
 
 # def my_function(param):
 #     print("Running my function")
